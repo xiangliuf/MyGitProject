016e445f7ba44f2dec1a248e26c66e3d7add755b
liuxiang <xiangliuf@isoftstone.com>
2018-03-30 17:04:49 +0800

    测试渣渣辉

---
+++ src/com/test/zzh/zzh.java
@@ -0,0 +1,23 @@
+/**  
+ * @Title:  zzh.java   
+ * @Package com.test.zzh   
+ * @Description:    TODO(用一句话描述该文件做什么)   
+ * @author: liuxiang     
+ * @date:   2018年3月30日 下午5:04:02   
+ * @version V1.0 
+ */  
+package com.test.zzh;
+
+/**   
+ * @ClassName:  zzh   
+ * @Description:TODO(这里用一句话描述这个类的作用)   
+ * @author: liuxiang 
+ * @date:   2018年3月30日 下午5:04:02 
+ * @version: V1.0   
+ */
+public class zzh {
+	public static void main(String[] args) {
+		System.out.println("打击");
+	}
+
+}
