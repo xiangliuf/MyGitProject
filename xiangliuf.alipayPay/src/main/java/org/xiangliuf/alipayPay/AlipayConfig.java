e76f4a618cfbeb614101f57a571895d7e47b29d7
xiangliuf <qudonghui>
2019-11-04 17:10:33 +0800

    支付宝沙箱环境对接

--- xiangliuf.alipayPay/src/main/java/org/xiangliuf/alipayPay/AlipayConfig.java
+++ xiangliuf.alipayPay/src/main/java/org/xiangliuf/alipayPay/AlipayConfig.java
@@ -1,72 +1,52 @@
-﻿package org.xiangliuf.alipayPay;
-
-import java.io.FileWriter;
-import java.io.IOException;
-
-/* *
- *类名：AlipayConfig
- *功能：基础配置类
- *详细：设置帐户有关信息及返回路径
- *修改日期：2017-04-05
- *说明：
- *以下代码只是为了方便商户测试而提供的样例代码，商户可以根据自己网站的需要，按照技术文档编写,并非一定要使用该代码。
- *该代码仅供学习和研究支付宝接口使用，只是提供一个参考。
- */
+package org.xiangliuf.alipayPay;
 
 public class AlipayConfig {
-	
-//↓↓↓↓↓↓↓↓↓↓请在这里配置您的基本信息↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
 
-	// 应用ID,您的APPID，收款账号既是您的APPID对应支付宝账号
-	public static String app_id = "";
-	
-	// 商户私钥，您的PKCS8格式RSA2私钥
-    public static String merchant_private_key = "";
-	
-	// 支付宝公钥,查看地址：https://openhome.alipay.com/platform/keyManage.htm 对应APPID下的支付宝公钥。
-    public static String alipay_public_key = "";
-
-	// 服务器异步通知页面路径  需http://格式的完整路径，不能加?id=123这类自定义参数，必须外网可以正常访问
-	public static String notify_url = "http://工程公网访问地址/alipay.trade.page.pay-JAVA-UTF-8/notify_url.jsp";
-
-	// 页面跳转同步通知页面路径 需http://格式的完整路径，不能加?id=123这类自定义参数，必须外网可以正常访问
-	public static String return_url = "http://工程公网访问地址/alipay.trade.page.pay-JAVA-UTF-8/return_url.jsp";
-
-	// 签名方式
-	public static String sign_type = "RSA2";
-	
-	// 字符编码格式
-	public static String charset = "utf-8";
-	
-	// 支付宝网关
-	public static String gatewayUrl = "https://openapi.alipay.com/gateway.do";
-	
-	// 支付宝网关
-	public static String log_path = "C:\\";
-
-
-//↑↑↑↑↑↑↑↑↑↑请在这里配置您的基本信息↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
-
-    /** 
-     * 写日志，方便测试（看网站需求，也可以改成把记录存入数据库）
-     * @param sWord 要写入日志里的文本内容
+    /**
+     * 正式环境请求地址
      */
-    public static void logResult(String sWord) {
-        FileWriter writer = null;
-        try {
-            writer = new FileWriter(log_path + "alipay_log_" + System.currentTimeMillis()+".txt");
-            writer.write(sWord);
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (writer != null) {
-                try {
-                    writer.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-}
+    public static String ALIPAY_URL = "https://openapi.alipay.com/gateway.do";
+    /**
+     * 支付宝分配给开发者的应用ID
+     *
+     */
+    public static String APP_ID = "2016101200667205";
+    //public static String APP_ID = "2019092367716873";
 
+    /**
+     * 支付接口名称
+     */
+    public static String PAY_METHOD = "alipay.trade.app.pay";
+    /**
+     * 提现接口名称
+     */
+    public static String TRANSFER_METHOD = "alipay.fund.trans.toaccount.transfer";
+    /**
+     * 仅支持JSON
+     */
+    public static String FORMAT = "JSON";
+    /**
+     * 请求使用的编码格式
+     */
+    public static String CHARSET = "utf-8";
+    /**
+     * 商户生成签名字符串所使用的签名算法类型
+     */
+    public static String SIGN_TYPE = "RSA2";
+    /**
+     * 支付宝公钥
+     */
+    public static String ALIPAY_PUBLIC_KEY = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDIgHnOn7LLILlKETd6BFRJ0GqgS2Y3mn1wMQmyh9zEyWlz5p1zrahRahbXAfCfSqshSNfqOmAQzSHRVjCqjsAw1jyqrXaPdKBmr90DIpIxmIyKXv4GGAkPyJ/6FTFY99uhpiq0qadD/uSzQsefWo0aTvP/65zi3eof7TcZ32oWpwIDAQAB";
+    //public static String ALIPAY_PUBLIC_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtM9e54TJ3iW8QabHNHkAErgda2df2mmkggC5iRErs41meQXojwj1gNtx8jCgwmtfnCbr7uGpFyp/u9I2C9SDVDa6381/gunPEchJnFpTFho8A2T5ZJeS1qdGP71usZpsC0w9QfXHk/pAwdecTyrTVqYRes1wdHyBDxM1t9IZNjLt3XDbdett5+eV5WU13dap2MF9IwtdXcfx8nNNrfFYfwrKVW2AUJqu13XVIJtpsEJTn40QZNXTVDjUv6vf8OJX1qCcdBRCxVmMRPmc4nopv6QRlPEFXGtUL4as01DKcOedWibO6gxRzRLeR1AQOEI7UHE0KsLDo3+KtIzhQwnEwQIDAQAB/u9I2C9SDVDa6381/gunPEchJnFpTFho8A2T5ZJeS1qdGP71usZpsC0w9QfXHk/pAwdecTyrTVqYRes1wdHyBDxM1t9IZNjLt3XDbdett5+eV5WU13dap2MF9IwtdXcfx8nNNrfFYfwrKVW2AUJqu13XVIJtpsEJTn40QZNXTVDjUv6vf8OJX1qCcdBRCxVmMRPmc4nopv6QRlPEFXGtUL4as01DKcOedWibO6gxRzRLeR1AQOEI7UHE0KsLDo3+KtIzhQwnEwQIDAQAB/6FTFY99uhpiq0qadD/uSzQsefWo0aTvP/65zi3eof7TcZ32oWpwIDAQAB";
+    /**
+     * 商户私钥
+     */
+    public static String PRIVATE_KEY = "MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCYmx0o1WgQLASWNRGXFXDFPzgsSPL7TpOAUWpJvMCkpMnqKC9qJsLGK9+QPDzsvAmOrvWbUghADHNSOWqb2/ktqDu/QI1b9q+/Mcgy5uws/nnuEgiz0o0REN+0wP3KZOi8r97LIpaeh7GIiLERLThQl/7997Ovl2uHdmiq5KQKz2oRQlCFyR2xB3FKv0pkrJd3rwErcHziy1RRYjHyP/RcRYeCwnjjaYoan5XsvrKaVnES8KmBjeI7R5J/fLjUrOu4FRBlAwqMf+8rLmzqdRpp/HTTm4yap1eh6Sth7vtA//fQWel4xBYVd5y3i35FsUNbkGtJB3UURzpEuEbULUs5AgMBAAECggEBAIp7uGplvpluWHs2jU/dVQip5f/J8zW6iH/ZYeqoLuk54UFsVhquMfJDvgzVnTP3L3UbsCXSu0nkAoKLOANYCzczg4R6v8zEf5exRUtr4sax51PzaZJFb14dQeR1fdk82TbhIfyU1rnlyWVtJcHAl2oLhIpTTev6LMow2aq68WIckAI0/xjK+Y7OHEVhTyuMfKxRPcGoiP5o+qcbZqRSptJCEIBGEN0tszLwaHiDwlEzSeW72A2rC7vYjNam4m09aty8Uz5vaj4Ixt5EId9MapyUm/Pcg+i59xspgmXB8SjB0g8EAEIVPdyNtUU3VhraWvSz067UvNz8gLExWLJOAAECgYEAyiEGfYI4OChvDGF6F0Ke6DTuQiPz13x41eMShpCErSIjvoScZHsIqhhe0J+D3u+xMb7nRBsMIPDc/9zM6h5b/6N5d5iS6UcgATAXmAH5dpmgbVWedCzyUJQiXgm6MdXnTf4TRf2yvNdGgJouMDfRnPH6C/BSRhPbZsCNyWI2CgECgYEAwUcyy9xVhKgJgSJOr0FXIu3Xf+3odHhbwK+lOBcIkwjrKDvUmS4v56ajAMTyZRq/BuH1brnl/khDRyeGuGEwp6Vqjz9xrTs8yI13HXw47az2Vhu9WsyfeJpX28+c5IIWRm81+XoC0RLj06XihmH3HFOfSjDukrjSdOUhSZF7ETkCgYAftvzYtpTxos6Bx82rVqWuU5fKwu9gsUQYc0+V05ktvTyUiSQs0+0xGy/NVioP0dYgsw6AbioCBSj38IIqfeb7fn/xlvv+JE3OQeiBzlWBGWvFm/iHzAGdSRD3ZllTtndbknEdu0fPP4vtY8O5hxiZopIUS3ntSvJS9gWJQqKuAQKBgQCTvMegOpCH8SYqtj2tCuX/Rgx2tjT8ak/ALsK86CZete+CdpFgcYSerqpkWSbrj5E7QpcnhyO9xiDfD+CTqX7WdsPIMXfEMcaZikDloeGmE5LMaTHJaSXwuxGzbLQBm0pJlpKMWu4yQB0z19YaYGHSHj1hKZwDPRFp1wclw/VtSQKBgBC5Emazys7fEnvDztNfjN1BXwmKZ3l7nmQFoxXnOheMJDWoFcmO9S3LAh/jv+R38nRJc+d1g3iToBxQzcxyOtfe1jlfzaFi0qH6ZJnf2b0MqR328FOod/47FfcBD8IoSQbWf60adkyKoHjUE4z5jI1nxS+A/OwcTcsoHNK9prWS";
+    //public static String PRIVATE_KEY = "MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCYmx0o1WgQLASWNRGXFXDFPzgsSPL7TpOAUWpJvMCkpMnqKC9qJsLGK9+QPDzsvAmOrvWbUghADHNSOWqb2/ktqDu/QI1b9q+/Mcgy5uws/nnuEgiz0o0REN+0wP3KZOi8r97LIpaeh7GIiLERLThQl/7997Ovl2uHdmiq5KQKz2oRQlCFyR2xB3FKv0pkrJd3rwErcHziy1RRYjHyP/RcRYeCwnjjaYoan5XsvrKaVnES8KmBjeI7R5J/fLjUrOu4FRBlAwqMf+8rLmzqdRpp/HTTm4yap1eh6Sth7vtA//fQWel4xBYVd5y3i35FsUNbkGtJB3UURzpEuEbULUs5AgMBAAECggEBAIp7uGplvpluWHs2jU/dVQip5f/J8zW6iH/ZYeqoLuk54UFsVhquMfJDvgzVnTP3L3UbsCXSu0nkAoKLOANYCzczg4R6v8zEf5exRUtr4sax51PzaZJFb14dQeR1fdk82TbhIfyU1rnlyWVtJcHAl2oLhIpTTev6LMow2aq68WIckAI0/xjK+Y7OHEVhTyuMfKxRPcGoiP5o+qcbZqRSptJCEIBGEN0tszLwaHiDwlEzSeW72A2rC7vYjNam4m09aty8Uz5vaj4Ixt5EId9MapyUm/Pcg+i59xspgmXB8SjB0g8EAEIVPdyNtUU3VhraWvSz067UvNz8gLExWLJOAAECgYEAyiEGfYI4OChvDGF6F0Ke6DTuQiPz13x41eMShpCErSIjvoScZHsIqhhe0J+D3u+xMb7nRBsMIPDc/9zM6h5b/6N5d5iS6UcgATAXmAH5dpmgbVWedCzyUJQiXgm6MdXnTf4TRf2yvNdGgJouMDfRnPH6C/BSRhPbZsCNyWI2CgECgYEAwUcyy9xVhKgJgSJOr0FXIu3Xf+3odHhbwK+lOBcIkwjrKDvUmS4v56ajAMTyZRq/BuH1brnl/khDRyeGuGEwp6Vqjz9xrTs8yI13HXw47az2Vhu9WsyfeJpX28+c5IIWRm81+XoC0RLj06XihmH3HFOfSjDukrjSdOUhSZF7ETkCgYAftvzYtpTxos6Bx82rVqWuU5fKwu9gsUQYc0+V05ktvTyUiSQs0+0xGy/NVioP0dYgsw6AbioCBSj38IIqfeb7fn/xlvv+JE3OQeiBzlWBGWvFm/iHzAGdSRD3ZllTtndbknEdu0fPP4vtY8O5hxiZopIUS3ntSvJS9gWJQqKuAQKBgQCTvMegOpCH8SYqtj2tCuX/Rgx2tjT8ak/ALsK86CZete+CdpFgcYSerqpkWSbrj5E7QpcnhyO9xiDfD+CTqX7WdsPIMXfEMcaZikDloeGmE5LMaTHJaSXwuxGzbLQBm0pJlpKMWu4yQB0z19YaYGHSHj1hKZwDPRFp1wclw/VtSQKBgBC5Emazys7fEnvDztNfjN1BXwmKZ3l7nmQFoxXnOheMJDWoFcmO9S3LAh/jv+R38nRJc+d1g3iToBxQzcxyOtfe1jlfzaFi0qH6ZJnf2b0MqR328FOod/47FfcBD8IoSQbWf60adkyKoHjUE4z5jI1nxS+A/OwcTcsoHNK9prWS";
+
+    /**
+     * 回调地址
+     */
+    public static String CALLBACK_URL = "http://10.242.129.65/uomp-uc/app/login.html";
+
+}
--- xiangliuf.alipayPay/src/test/java/org/xiangliuf/alipayPay/AppTest.java
+++ xiangliuf.alipayPay/src/test/java/org/xiangliuf/alipayPay/AppTest.java
@@ -1,5 +1,13 @@
 package org.xiangliuf.alipayPay;
 
+import com.alipay.api.AlipayApiException;
+import com.alipay.api.AlipayClient;
+import com.alipay.api.DefaultAlipayClient;
+import com.alipay.api.domain.AlipayTradeAppPayModel;
+import com.alipay.api.request.AlipayOpenPublicTemplateMessageIndustryModifyRequest;
+import com.alipay.api.request.AlipayTradeAppPayRequest;
+import com.alipay.api.response.AlipayOpenPublicTemplateMessageIndustryModifyResponse;
+import com.alipay.api.response.AlipayTradeAppPayResponse;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -7,32 +15,52 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest  extends TestCase {
+
+    //应用id
+    private static final String APP_ID = "2016101200667205";
+    //应用私钥
+    private static final String APP_PRIVATE_KEY = "MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCYmx0o1WgQLASWNRGXFXDFPzgsSPL7TpOAUWpJvMCkpMnqKC9qJsLGK9+QPDzsvAmOrvWbUghADHNSOWqb2/ktqDu/QI1b9q+/Mcgy5uws/nnuEgiz0o0REN+0wP3KZOi8r97LIpaeh7GIiLERLThQl/7997Ovl2uHdmiq5KQKz2oRQlCFyR2xB3FKv0pkrJd3rwErcHziy1RRYjHyP/RcRYeCwnjjaYoan5XsvrKaVnES8KmBjeI7R5J/fLjUrOu4FRBlAwqMf+8rLmzqdRpp/HTTm4yap1eh6Sth7vtA//fQWel4xBYVd5y3i35FsUNbkGtJB3UURzpEuEbULUs5AgMBAAECggEBAIp7uGplvpluWHs2jU/dVQip5f/J8zW6iH/ZYeqoLuk54UFsVhquMfJDvgzVnTP3L3UbsCXSu0nkAoKLOANYCzczg4R6v8zEf5exRUtr4sax51PzaZJFb14dQeR1fdk82TbhIfyU1rnlyWVtJcHAl2oLhIpTTev6LMow2aq68WIckAI0/xjK+Y7OHEVhTyuMfKxRPcGoiP5o+qcbZqRSptJCEIBGEN0tszLwaHiDwlEzSeW72A2rC7vYjNam4m09aty8Uz5vaj4Ixt5EId9MapyUm/Pcg+i59xspgmXB8SjB0g8EAEIVPdyNtUU3VhraWvSz067UvNz8gLExWLJOAAECgYEAyiEGfYI4OChvDGF6F0Ke6DTuQiPz13x41eMShpCErSIjvoScZHsIqhhe0J+D3u+xMb7nRBsMIPDc/9zM6h5b/6N5d5iS6UcgATAXmAH5dpmgbVWedCzyUJQiXgm6MdXnTf4TRf2yvNdGgJouMDfRnPH6C/BSRhPbZsCNyWI2CgECgYEAwUcyy9xVhKgJgSJOr0FXIu3Xf+3odHhbwK+lOBcIkwjrKDvUmS4v56ajAMTyZRq/BuH1brnl/khDRyeGuGEwp6Vqjz9xrTs8yI13HXw47az2Vhu9WsyfeJpX28+c5IIWRm81+XoC0RLj06XihmH3HFOfSjDukrjSdOUhSZF7ETkCgYAftvzYtpTxos6Bx82rVqWuU5fKwu9gsUQYc0+V05ktvTyUiSQs0+0xGy/NVioP0dYgsw6AbioCBSj38IIqfeb7fn/xlvv+JE3OQeiBzlWBGWvFm/iHzAGdSRD3ZllTtndbknEdu0fPP4vtY8O5hxiZopIUS3ntSvJS9gWJQqKuAQKBgQCTvMegOpCH8SYqtj2tCuX/Rgx2tjT8ak/ALsK86CZete+CdpFgcYSerqpkWSbrj5E7QpcnhyO9xiDfD+CTqX7WdsPIMXfEMcaZikDloeGmE5LMaTHJaSXwuxGzbLQBm0pJlpKMWu4yQB0z19YaYGHSHj1hKZwDPRFp1wclw/VtSQKBgBC5Emazys7fEnvDztNfjN1BXwmKZ3l7nmQFoxXnOheMJDWoFcmO9S3LAh/jv+R38nRJc+d1g3iToBxQzcxyOtfe1jlfzaFi0qH6ZJnf2b0MqR328FOod/47FfcBD8IoSQbWf60adkyKoHjUE4z5jI1nxS+A/OwcTcsoHNK9prWS";
+    //字符集
+    private static final String CHARSET = "UTF-8";
+    //支付宝公钥
+    private static final String ALIPAY_PUBLIC_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtM9e54TJ3iW8QabHNHkAErgda2df2mmkggC5iRErs41meQXojwj1gNtx8jCgwmtfnCbr7uGpFyp";
+
 
     /**
-     * @return the suite of tests being tested
+     * 支付接口調用
+     * @throws AlipayApiException
      */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    public void testPay() throws AlipayApiException {
+
+        // 实例化客户端
+        AlipayClient alipayClient = new DefaultAlipayClient(AlipayConfig.ALIPAY_URL, AlipayConfig.APP_ID,
+                AlipayConfig.PRIVATE_KEY, AlipayConfig.FORMAT, AlipayConfig.CHARSET, AlipayConfig.ALIPAY_PUBLIC_KEY,
+                AlipayConfig.SIGN_TYPE);
+        // 实例化具体API对应的request类,类名称和接口名称对应,当前调用接口名称：alipay.trade.app.pay
+        AlipayTradeAppPayRequest request = new AlipayTradeAppPayRequest();
+        // SDK已经封装掉了公共参数，这里只需要传入业务参数。以下方法为sdk的model入参方式(model和biz_content同时存在的情况下取biz_content)。
+        AlipayTradeAppPayModel model = new AlipayTradeAppPayModel();
+        model.setBody("用户充值");
+        model.setSubject("XX平台APP");
+        // 唯一订单号 根据项目中实际需要获取相应的
+        model.setOutTradeNo("201909270001");// outtradeno 生存订单
+        // 支付超时时间（根据项目需要填写）
+        model.setTimeoutExpress("60m");
+        // 支付金额（项目中实际订单的需要支付的金额，金额的获取与操作请放在服务端完成，相对安全）
+        model.setTotalAmount("11");
+        model.setProductCode("QUICK_MSECURITY_PAY");
+        request.setBizModel(model);
+        // 支付成功后支付宝异步通知的接收地址url
+        request.setNotifyUrl(AlipayConfig.CALLBACK_URL);//异步回调url
+
+        // 这里和普通的接口调用不同，使用的是sdkExecute
+        AlipayTradeAppPayResponse response = alipayClient.sdkExecute(request);
+        System.out.println(response.getBody());//就是orderString 可以直接给客户端请求，无需再做处理。
+        //调用成功，则处理业务逻辑
+        if(response.isSuccess()){
+            //.....
+        }
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
